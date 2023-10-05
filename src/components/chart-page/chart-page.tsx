43e79e0ddda87d484999a376e917a9ab27b0a75d
Kilian Glomb <kglomb@2orgu.com>
2023-10-05 14:51:13 +0200

    styling

--- src/components/chart-page/chart-page.tsx
+++ src/components/chart-page/chart-page.tsx
@@ -34,13 +34,14 @@ export class ChartPage {
       ],
     };
 
-    // let formatCount = d3.format(',.0f');
-
     let margin = { top: 10, right: 30, bottom: 30, left: 30 },
       width = 460 - margin.left - margin.right,
       height = 300 - margin.top - margin.bottom;
 
+    // Declare the x (horizontal position) scale.
     let x = d3.scaleLinear().rangeRound([0, width]);
+
+    // Declare the y (vertical position) scale.
     let y = d3
       .scaleLinear()
       .domain([
@@ -51,11 +52,13 @@ export class ChartPage {
       ])
       .range([height, 0]);
 
+    // Create the SVG container.
     let svg = d3
       .select('body')
       .append('svg')
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
+      .attr('fill', 'steelblue')
       .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
