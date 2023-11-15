5fbba4d91785238f5fc8b09b97b6498644acb82e
Kilian Glomb <kglomb@2orgu.com>
2023-11-15 15:47:06 +0100

    charte index fixed

--- src/components/chart-index/chart-index.tsx
+++ src/components/chart-index/chart-index.tsx
@@ -20,14 +20,15 @@ export class ChartIndex {
   }
 
   componentDidLoad() {
+    //Date has to be sorted ascending
     const jsonData = [
-      { date: new Date('2022-07-02'), value: 250 },
-      { date: new Date('2022-03-02'), value: 350 },
-      { date: new Date('2022-01-02'), value: 450 },
-      { date: new Date('2022-01-02'), value: 550 },
-      { date: new Date('2022-08-02'), value: 650 },
-      { date: new Date('2022-01-02'), value: 750 },
-      { date: new Date('2022-12-02'), value: 850 },
+      { date: new Date('2022-01-01'), value: 250 },
+      { date: new Date('2022-02-01'), value: 450 },
+      { date: new Date('2022-03-01'), value: 350 },
+      { date: new Date('2022-04-01'), value: 650 },
+      { date: new Date('2022-05-01'), value: 550 },
+      { date: new Date('2022-06-01'), value: 850 },
+      { date: new Date('2022-07-01'), value: 750 },
     ];
 
     const desiredHeight: number = 448;
@@ -40,11 +41,12 @@ export class ChartIndex {
     const x = d3.scaleTime().range([0, width]);
     const y = d3.scaleLinear().range([height, 0]);
 
+    // create svg
     const svg = d3
       .select(this.svgContainer)
       .append('svg')
-      .attr('width', width)
-      .attr('height', height)
+      .attr('width', width + padding * 2)
+      .attr('height', height + padding * 2)
       .append('g')
       .attr('transform', 'translate(' + padding + ',' + padding + ')');
 
