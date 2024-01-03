ba03bc2a758113ce9d1ad4c99705850e72ba96c7
Kilian Glomb <kglomb@2orgu.com>
2024-01-03 16:56:17 +0100

    variables

--- src/components/page-footer/page-footer.tsx
+++ src/components/page-footer/page-footer.tsx
@@ -10,17 +10,19 @@ export class PageFooter {
     return (
       <Host>
         <div class="grid">
+          <div class="column"></div>
           <div class="column">
-            <div>Bachelorarbeit von Kilian Glomb</div>
-            <div>MatrikelNr.: 6026140</div>
-            <div>kilian.glomb@student.jade-hs.de</div>
-          </div>
-          <div class="column">
+            <h2>Institution</h2>
             <div>Jade Hochhochschule Oldenburg</div>
             <div>Ofener Str. 16/19</div>
             <div>Oldenburg</div>
           </div>
-          <div class="column"></div>
+          <div class="column">
+            <h2>Author</h2>
+            <div>Kilian Glomb</div>
+            <div>Matrikel-Nr.: 6026140</div>
+            <div>E-Mail: kilian.glomb@student.jade-hs.de</div>
+          </div>
         </div>
       </Host>
     );
