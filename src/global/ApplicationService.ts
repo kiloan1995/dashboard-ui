6c93525d77010c3b312f6c5141a21fa89bf1b503
Kilian Glomb <kglomb@2orgu.com>
2024-01-05 15:47:09 +0100

    refactored applicationservice

--- src/global/ApplicationService.ts
+++ src/global/ApplicationService.ts
@@ -4,52 +4,32 @@ import fetch from 'node-fetch';
 
 export class ApplicationService {
   static async getApplication(appId: string): Promise<Application | undefined> {
-    var myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
-    let data = { applicationId: appId };
-
-    let requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: JSON.stringify(data),
-      redirect: 'follow',
-    };
-    const url = 'http://127.0.0.1:5001/dashboardui-rs/' + ServerSettings.serverRegion + '/getApplication';
-    const response = await fetch(url, requestOptions);
-    let app: Application = undefined;
-    try {
-      let text = await response.text();
-      let json = JSON.parse(text);
-      app = json['application'] as Application; //unwrap promise
-      console.log('getApplication Success:', app);
-    } catch (e) {
-      console.log('getApplication failed:', e);
-    }
-    return app;
+    return (await this.doRequest('application/json', { applicationId: appId }, 'getApplication', 'application')) as Application;
   }
 
   static async getApplicationsAtJob(jobId: string): Promise<Application[] | undefined> {
+    return (await this.doRequest('application/json', { jobId: jobId }, 'getAllApplicationsAtJob', 'applications')) as Application[];
+  }
+
+  static async doRequest(contentType: string, data: any, functionName: string, key: string): Promise<any | null> {
     let myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
-    let data = { jobId: jobId };
+    myHeaders.append('Content-Type', contentType);
     let requestOptions = {
       method: 'POST',
       headers: myHeaders,
       body: JSON.stringify(data),
       redirect: 'follow',
     };
-    const url = 'http://127.0.0.1:5001/dashboardui-rs/' + ServerSettings.serverRegion + '/getAllApplicationsAtJob';
+    const url = 'http://127.0.0.1:5001/dashboardui-rs/' + ServerSettings.serverRegion + '/' + functionName;
     const response = await fetch(url, requestOptions);
-    let apps: Application[] = undefined;
     try {
       let text = await response.text();
       console.log('getApplication Success:', text);
       let json = JSON.parse(text);
-      apps = json['applications'] as Application[]; //unwrap promise
-      console.log('getApplications Success:', apps);
+      return json[key];
     } catch (e) {
       console.log('getApplications failed:', e);
     }
-    return apps;
+    return null;
   }
 }
